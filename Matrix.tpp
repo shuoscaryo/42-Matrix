1100dad8d72003b0a3af1b6b0d6d492bf70fa8e6
shuoscaryo <shuoscaryo@hgmail.com>
2025-12-06 19:02:45 +0100

    [main]<2025-12-06-19:02> quicksave

--- Matrix.tpp
+++ Matrix.tpp
@@ -6,6 +6,8 @@
 #include <initializer_list>
 #include <utility>
 #include "Vector.tpp"
+#define _USE_MATH_DEFINES
+#include <cmath>
 
 template <typename T> class Matrix;
 
@@ -62,7 +64,6 @@ class Matrix
 		Matrix<T> inverse() const;
 
 		size_t rank() const;
-
 
 	protected:
 	private:
@@ -622,3 +623,20 @@ size_t Matrix<T>::rank() const
 }
 
 // EX14
+
+Matrix<float> projection(float fov, float ratio, float near, float far)
+{
+	// alfa is YZ angle, beta is XZ angle
+	float tanBeta = tan(fov/2);
+	float tanAlfa = ratio * tanBeta;
+	// Z projection ratios
+    float K = far / (near - far);  // <-- sin el signo menos delante
+    float L = near * far / (near - far);
+	Matrix<float> output = {
+		{1/tanAlfa,0,0,0},
+		{0,1/tanBeta,0,0},
+		{0,0,K,L},
+		{0,0,-1,0}
+	};
+	return output;
+}
--- ex14/main.cpp
+++ ex14/main.cpp
@@ -1,6 +1,51 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "Matrix.tpp"
+#include "Vector.tpp"
+#include "Test.tpp"
+
+
+void helper(float fov, float ratio, float near, float far)
+{
+	Matrix<float> A = projection(fov, ratio, near, far);
+	std::cout << "fov: " << fov * 180/M_PI << "º ,ratio: " << ratio << " ,near: "
+		<< near << " ,far: " << far << "\n";
+	std::cout << "projection matrix:\n"
+		<< A << "\n";
+	for (size_t row = 0; row < A.shape().first; ++row)
+	{
+		for (size_t col = 0; col < A.shape().second; ++col)
+		{
+			std::cout << A[col][row];
+			if (col + 1 != A.shape().first)
+				std::cout << ", ";
+		}
+		if (row + 1 != A.shape().second)
+			std::cout << "\n";
+	}	
+}
+
+void myTests()
+{
+	Test::header("my tests");
+	Test::add("",
+		helper,
+		M_PI/2,
+		1,
+		1,
+		2
+	);
+}
+
+void subjectTests()
+{
+	Test::header("subject tests");
+}
 
 int main()
 {
-
+	subjectTests();
+	myTests();
 	return 0;
 }
